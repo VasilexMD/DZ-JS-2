5334ae0784730346fe9506727545424e80a1e3dc
VasilexMD <124679335+VasilexMD@users.noreply.github.com>
2023-04-26 13:26:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// const number = prompt("Введите число:");
+// const tenPercent = number * 0.1;
+// console.log(`10% от ${number} равно ${tenPercent}`);
+
+
+// const a = prompt("Введите первое число:");
+// const b = prompt("Введите второе число:");
+// if (a < b) {
+//   console.log(`${a} меньше, чем ${b}`);
+// } else if (a > b) {
+//   console.log(`${b} меньше, чем ${a}`);
+// } else {
+//   console.log(`${a} и ${b} равны`);
+// }
+
+
+// const number = prompt("Введите число:");
+// if (number < 100) {
+//   console.log("Число меньше 100");
+// } else if (number > 100) {
+//   console.log("Число больше 100");
+// } else {
+//   console.log("Число равно 100");
+// }
+
+
+// const name = prompt("Введите свое имя:");
+// const age = prompt("Введите свой возраст:");
+
+// if (age >= 18) {
+//   console.log(`Hello, ${name}`);
+// } else {
+//   console.log(`Hi, ${name}`);
+// }
